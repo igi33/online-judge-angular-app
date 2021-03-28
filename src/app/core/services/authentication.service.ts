7f178296a80c44def9cef65025052c49313d8261
igi33 <stosicigor96@gmail.com>
2021-03-29 01:12:05 +0200

    Updated jwt-decode + accompanying change
    This was needed to fix a warning.

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import * as jwt_decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 import { User } from '../../shared/models/User';
 
 @Injectable({
